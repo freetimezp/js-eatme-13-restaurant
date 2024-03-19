a10e01c1e079945149d41612a40e0616e985318d
freetimezp <kraftukrnet@ukr.net>
2024-03-19 20:41:36 +0200

    18. js-eatme-13-restaurant. Scroll to sections. Set active nav on scroll.

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import './nav.css';
+import { useRouter, usePathname } from 'next/navigation';
 
 import { navs } from '../data/data';
 
@@ -8,6 +9,9 @@ const Nav = () => {
     const [navList, setNavList] = useState(navs);
     const [open, setOpen] = useState(false);
     const [scroll, setScroll] = useState(0);
+
+    const pathname = usePathname();
+    const router = useRouter();
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -26,16 +30,45 @@ const Nav = () => {
     }
 
     const handleScrollTo = (section: string) => {
+        let header: HTMLElement = document.querySelector('#header')!;
+        let offset = header.offsetHeight;
+        let targetEl: HTMLElement = document.querySelector('#' + section)!;
 
+        if (pathname === '/') {
+            let elementPosition = targetEl.offsetTop;
+            window.scrollTo({
+                top: elementPosition - offset,
+                behavior: 'smooth'
+            });
+        } else {
+            router.push(`/#${section}`);
+        }
     }
 
     const handleNavActive = () => {
+        let position = scroll + 200;
+        setNavList(
+            navList.map(nav => {
+                nav.active = false;
+                let targetSection: HTMLElement = document.querySelector('#' + nav.target)!;
 
+                if (targetSection
+                    && position >= targetSection.offsetTop
+                    && position <= targetSection.offsetTop + targetSection.offsetHeight) {
+                    nav.active = true;
+                }
+
+                return nav;
+            })
+        );
     }
 
+    useEffect(() => {
+        handleNavActive();
+    }, [scroll]);
+
     return (
-        <nav id='navbar' className={`navbar order-last order-lg-0 
-            ${open ? 'navbar-mobile' : undefined}`}>
+        <nav id='navbar' className={`navbar order-last order-lg-0 ${open ? 'navbar-mobile' : undefined}`}>
             <ul>
                 {navList.map((nav) => (
                     <li key={nav.id}>
